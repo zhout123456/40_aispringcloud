47285d42529f7ad5a9cd7a4aab5736308cca889e
JoyZhou <752074541@qq.oom>
2020-05-08 20:54:57 +0800

    20200508:1.更新aispringcloud

---
+++ hystrix/src/main/java/com/southwind/HystrixApplication.java
@@ -0,0 +1,19 @@
+package com.southwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+/*声明启用数据监控*/
+@EnableCircuitBreaker
+/*声明启用可视化数据监控*/
+@EnableHystrixDashboard
+public class HystrixApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class,args);
+    }
+}
